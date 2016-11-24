f8deabbd2bed40504d0da437321a85a9dc922d49
rahul.sapkal <rahul.sapkal@neosofttech.com>
2016-11-24 16:37:47 +0530

    trying to build GUI of all app but first go for NeoSTORE API

--- app/controllers/HomeScreen.js
+++ app/controllers/HomeScreen.js
@@ -3,3 +3,8 @@ $.header.__views.tital.text = "Home Screen";
 $.header.__views.back.addEventListener('click', function(e) {
     $.win.close();
 });
+
+function ListProduct(e){
+  var ListProduct=Alloy.createController('ListProduct').getView();
+  ListProduct.open();
+}
