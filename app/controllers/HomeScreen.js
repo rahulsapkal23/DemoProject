2cba5853d3555f2ead9346a6aeac58335f06fe59
rahul.sapkal <rahul.sapkal@neosofttech.com>
2016-12-02 19:04:49 +0530

    scroll my profile

--- app/controllers/HomeScreen.js
+++ app/controllers/HomeScreen.js
@@ -1,9 +1,80 @@
 // ######################################### Setting Header #########################################
 $.header.__views.tital.text = "Home Screen";
+$.header.__views.back.text="\uf0c9";
 $.header.__views.back.addEventListener('click', function(e) {
-    $.HomeScreenwin.close();
+    // $.HomeScreenwin.close();
+
+    SlideToMyProfile(e);
 });
 
+
+
+function SlideToMyProfile(e) {
+  Ti.API.info("inside slider");
+
+
+ //// ---- Menu window, positioned on the left
+ // var menuWindow = Ti.UI.createWindow({
+ //     top:0,
+ //     left:0,
+ //     width:"550",
+ //     backgroundColor:"green"
+ // });
+
+
+ // menuWindow.open();
+
+ if(e.source.toggle == true){
+Ti.API.info("inside if");
+// menuWindow.close();
+       $.view1.animate({
+           left:0,
+           duration:400,
+           curve:Ti.UI.ANIMATION_CURVE_EASE_IN_OUT
+       });
+       $.view2.animate({
+           left:"-500",
+           duration:400,
+           curve:Ti.UI.ANIMATION_CURVE_EASE_IN_OUT
+       });
+       e.source.toggle = false;
+   }
+   // If the menu isn't opened
+   else{
+      // menuWindow.open();
+     Ti.API.info("inside else");
+
+       $.view1.animate({
+           left:"500",
+           duration:400,
+           curve:Ti.UI.ANIMATION_CURVE_EASE_IN_OUT
+       });
+       $.view2.animate({
+           left:"0",
+           duration:400,
+           curve:Ti.UI.ANIMATION_CURVE_EASE_IN_OUT
+       });
+      // menuWindow.animate({
+      //      left:"-85",
+      //      duration:400,
+      //      curve:Ti.UI.ANIMATION_CURVE_EASE_IN_OUT
+      //  });
+       e.source.toggle  = true;
+   }
+}
+$.view1.addEventListener('swipe',function(e){
+
+  // $.HomeScreenwin.animate({
+  //     left:"500",
+  //     duration:400,
+  //     curve:Ti.UI.ANIMATION_CURVE_EASE_IN_OUT
+  // });
+  // $.view2.animate({
+  //     left:"-200",
+  //     duration:400,
+  //     curve:Ti.UI.ANIMATION_CURVE_EASE_IN_OUT
+  // });
+});
 // $.HomeScreenwin.orientationModes = [Ti.UI.PORTRAIT, Ti.UI.UPSIDE_PORTRAIT]
 // ################################# get argument from previous controllew ###################################
 var access_token = arguments[0] || {};
@@ -34,7 +105,7 @@ var client = Ti.Network.createHTTPClient({
         Ti.API.info(" onerror" + JSON.stringify(e));
         Ti.API.info("client.responseText onerror" + client.getResponseText());
         Ti.API.info(response.message);
-        alert(response.message); 
+        alert(response.message);
     },
     //  timeout : 5000  // in milliseconds
 });
@@ -61,3 +132,53 @@ function HomeScreenDetails(LoginDetails) {
 
 
 }
+
+
+
+//
+//  $.HomeScreenwin.addEventListener('touchstart', function(e){
+//     // Get starting horizontal position
+//     e.source.axis = parseInt(e.x);
+// });
+//
+//
+// $.HomeScreenwin.addEventListener('touchmove', function(e){
+//     // Subtracting current position to starting horizontal position
+//     var coordinates = parseInt(e.globalPoint.x) - e.source.axis;
+//     // Detecting movement after a 20px shift
+//     if(coordinates > 20 || coordinates < -20){
+//         e.source.moving = true;
+//     }
+//     // Locks the window so it doesn't move further than allowed
+//     if(e.source.moving == true && coordinates <= 550 && coordinates >= 0){
+//         // This will smooth the animation and make it less jumpy
+//         $.HomeScreenwin.animate({
+//             left:coordinates,
+//             duration:20
+//         });
+//         // Defining coordinates as the final left position
+//         $.HomeScreenwin.left = coordinates;
+//     }
+// });
+
+
+
+// $.HomeScreenwin.addEventListener('touchend', function(e){
+//     // No longer moving the window
+//     e.source.moving = false;
+//     if($.HomeScreenwin.left >= 75 && $.HomeScreenwin.left < 550){
+//         // Repositioning the window to the right
+//         $.HomeScreenwin.animate({
+//             left:550,
+//             duration:300
+//         });
+//         $.header.__views.back.toggle = true;
+//     }else{
+//         // Repositioning the window to the left
+//         $.HomeScreenwin.animate({
+//             left:0,
+//             duration:300
+//         });
+//         $.header.__views.back.toggle = false;
+//     }
+// });
